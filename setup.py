f1d4102d034d4f24bea04f02ce2d1865b9c600ac
skushwaha <C69IFjez2*L@>
2020-08-09 14:34:26 +0530

    0.1.3

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ README = (HERE / "README.md").read_text()
 setup(
     name='viper-python',
     packages=['viper'],
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     description='Common utility functions.',
     author='Siddhant Kushwaha',
     author_email='k16.siddhant@gmail.com',
     url='https://github.com/siddhantkushwaha/viper',
-    download_url='https://github.com/siddhantkushwaha/viper/archive/0.1.2.tar.gz',
+    download_url='https://github.com/siddhantkushwaha/viper/archive/0.1.3.tar.gz',
     keywords=['DOWNLOAD', 'PARALLEL', 'CONCURRENT'],
     install_requires=[
         'requests',
